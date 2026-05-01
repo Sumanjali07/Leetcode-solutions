56ef40243842a7cd3ae1b464af9bdaacda8f6058
Thota Sumanjali <sumanjalithota1476@gmail.com>
2026-05-01 21:52:37 +0530

    Time: 0 ms (100%), Space: 43.1 MB (82.47%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length==0) return "";
+        String prefix=strs[0];//initialize prefix to 1st word of strs list or array
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
